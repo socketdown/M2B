7d6c2b35431c810848ae391985e9e96028be81eb
Socketdown <romanhim@gmail.com>
2016-12-02 11:40:54 +0100

    port 3

--- Magic2Brain/www/js/optionHandler.js
+++ Magic2Brain/www/js/optionHandler.js
@@ -6,12 +6,15 @@ ajapp.controller('OptionController', function( $scope, Option, MBar, CurrentPage
 	$scope.dloptions = { image_autoupdate: "Daily", caching: true };
 
 	$scope.about = {};
-/*
+
 	$scope.imagepacks = [
 		{ name: "Dunno", selected: false, loaded: false}
 	];
 
-	$scope.selectAllImagepacks = false;
+	$scope.pbar1 = $scope.dloptions.caching;
+
+	/*
+	$scope.selectAllImagePacks = false;
 	if( $scope.selectAllImagePacks ){
 		for( var i = 0; i < $scope.imagepacks.length; i++ ){
 			$scope.imagepacks.[i].selected = true;
